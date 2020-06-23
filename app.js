af0f1f34cd8aec0b482369da843c0066b1521eb0
pradhan <techbulletinprime@gmail.com>
2020-06-23 14:47:06 +0530

    added express module

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+const express=require('express')
+const path=require('path')
+const app=express()
+app.use(express.static(path.join(__dirname,'./views')))
+//pp.set('views')
+app.get('/abc',(req,res)=>{
+
+    return res.send("Hi")
+})
 const validator=require('validator')
 const email='rajanikamta@gmail.com'
 debugger
 console.log(validator.isEmail(email))
+app.listen(3000)
